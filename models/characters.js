f53b3512b3043aff99672731e350aafac5541430
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2024-05-14 20:33:29 -0500

    Eliminación de información que no se usa con la nueva información de la API

--- models/characters.js
+++ models/characters.js
@@ -1,83 +1,76 @@
-export class Characters {
-    _listCharacters = {}
-    _URL = 'https://api.sampleapis.com/rickandmorty/characters'
+const URL = 'https://api.sampleapis.com/rickandmorty/characters'
 
-    constructor() {
-        this._listCharacters = {}
-    }
+export const createCharacter = async({ name, status, species, type, gender, origin, image }) => {
+    await fetch(`${URL}`, {
+        method: "POST",
+        body: JSON.stringify({ name, status, species, type, gender, origin, image }),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json()) 
+        .then(json => console.log(json))
+        .catch(error => console.log(error))
+}
 
-    async createCharacter({ name, status, species, type, gender, origin, image }) {
-        await fetch(`${this._URL}`, {
-            method: "POST",
-            body: JSON.stringify({ name, status, species, type, gender, origin, image }),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-            })
-            .then(response => response.json()) 
-            .then(json => console.log(json))
-            .catch(error => console.log(error))
-    }
+export const convertToArray = async() => {
+    const list = []
 
-    async convertToArray() {
-        const list = []
+    await fetch(`${URL}`)
+        .then(response => response.json())
+        .then(json => {
+            Object.keys(json).forEach(key => list.push(json[key]))
+        })
+        .catch(error => console.log('Solicitud fallida'.red, error))
 
-        await fetch(`${this._URL}`)
-            .then(response => response.json())
-            .then(json => {
-                Object.keys(json).forEach(key => list.push(json[key]))
-            })
-            .catch(error => console.log('Solicitud fallida'.red, error))
+    return list
+}
 
-        return list
-    }
+export const showCharacters = async() => {
+    await fetch(`${URL}`)
+        .then(response => response.json())
+        .then(json => {
+            console.log('\n')
+            json.forEach((character, idx) => {
+            const index = `${idx + 1}.`.blue
+            const { name, status, type, gender, origin } = character
+            console.log(`${index} ${name}, ${status}, ${type}, ${gender}, born on ${origin}`)
+        })
+        })
+        .catch(error => console.log('Solicitud fallida'.red, error))
+}
 
-    async showCharacters() {
-        await fetch(`${this._URL}`)
-            .then(response => response.json())
-            .then(json => {
-                console.log('\n')
-                json.forEach((character, idx) => {
-                const index = `${idx + 1}.`.blue
-                const { name, status, type, gender, origin } = character
-                console.log(`${index} ${name}, ${status}, ${type}, ${gender}, born on ${origin}`)
-            })
-            })
-            .catch(error => console.log('Solicitud fallida'.red, error))
-    }
+export const deleteCharacter = async(id) => {
+    await fetch(`${URL}/${id}`, {
+        method: "DELETE",
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json())
+        .catch(error => console.log(error))
+}
 
-    async deleteCharacter(id) {
-        await fetch(`${this._URL}/${id}`, {
-            method: "DELETE",
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-            })
-            .then(response => response.json())
-            .catch(error => console.log(error))
-    }
+export const updateCharacter = async({ id, name, status, species, type, gender, origin, image }) => {
+    await fetch(`${URL}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ name, status, species, type, gender, origin, image }),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json()) 
+        .then(json => console.log(json))
+        .catch(error => console.log(error))
+}
 
-    async updateCharacter({ id, name, status, species, type, gender, origin, image }) {
-        await fetch(`${this._URL}/${id}`, {
-            method: "PUT",
-            body: JSON.stringify({ name, status, species, type, gender, origin, image }),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-            })
-            .then(response => response.json()) 
-            .then(json => console.log(json))
-            .catch(error => console.log(error))
-    }
+export const findByIdCharacter = async(id) => {
+    await fetch(`${URL}/${id}`)
+        .then(response => response.json())
+        .then(json => console.log('\n', json))
+        .catch(error => console.log('Solicitud fallida'.red, error))
+}
 
-    async findByIdCharacter(id) {
-        await fetch(`${this._URL}/${id}`)
-            .then(response => response.json())
-            .then(json => console.log('\n', json))
-            .catch(error => console.log('Solicitud fallida'.red, error))
-    }
-
-    async findCharactersInformation(attribute, value) {
-        await fetch(`${this._URL}/?${attribute}=${value}`)
-            .then(response => response.json())
-            .then(json => {
-                const characters = json.filter(character => character[attribute] === value)
-                console.log('\n', characters)
-            })
-            .catch(error => console.log('Solicitud fallida'.red, error))
-    }
+export const findCharactersInformation = async(attribute, value) => {
+    await fetch(`${URL}/?${attribute}=${value}`)
+        .then(response => response.json())
+        .then(json => {
+            const characters = json.filter(character => character[attribute] === value)
+            console.log('\n', characters)
+        })
+        .catch(error => console.log('Solicitud fallida'.red, error))
 }
